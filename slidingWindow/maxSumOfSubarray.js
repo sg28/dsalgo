93477b8bdf34e0586a21a7ade9e4d6e851b6f947
sg28 <g.snehasish@gmail.com>
2023-12-25 14:46:29 -0800

    Basic Sliding Window Concept

---
+++ slidingWindow/maxSumOfSubarray.js
@@ -0,0 +1,10 @@
+/*
+    Given an array of integers and a fixed window size, find the maximum sum of any sub array
+
+*/ 
+let maxSumOfSubarray = (nums, k)=>{
+    console.log('  ',nums);
+}
+let nums = [4,2,1,7,8,1,2,8,1,0];
+let k = 3;
+maxSumOfSubarray(nums, k)
