3878bdc446e2de261574109e062fd3da50978ea1
sg28 <g.snehasish@gmail.com>
2024-05-16 11:02:13 -0700

    add description

--- multiplyStrings.js
+++ multiplyStrings.js
@@ -1,3 +1,12 @@
+/*
+Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2, also represented as a string.
+Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+
+Example 1:
+Input: num1 = "2", num2 = "3"
+Output: "6"
+*/
+
 let multiplyStrings = (num1, num2) => {
   let m = num1.length;
   let n = num2.length;
@@ -12,7 +21,7 @@ let multiplyStrings = (num1, num2) => {
       res[i + j] += Math.floor(sum / 10);
     }
   }
-  let resultString = res.join('');
+  let resultString = res.join("");
   let finalString = removeLeadingZero(resultString);
   return finalString || "0";
 };
